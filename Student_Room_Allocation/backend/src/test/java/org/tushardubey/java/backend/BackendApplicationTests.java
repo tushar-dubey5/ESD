06c24fda1bf7e0d9c057be3da95abc76b6c2765b
Tushar Dubey <dubeytushar316@gmail.com>
2024-11-19 21:51:21 +0530

    Initialization and Basic setup for Mini Project

---
+++ Student_Room_Allocation/backend/src/test/java/org/tushardubey/java/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tushardubey.java.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
